753530b46f45844967fcb7890254f193b60bfb3f
Lucas <lucassoares2007@hotmail.com>
2023-01-23 12:14:20 -0300

    perf

---
+++ main.py
@@ -0,0 +1,23 @@
+minha_string = 'Lucas'
+print(type(minha_string))
+
+meu_inteiro = 1234
+print(type(meu_inteiro))
+
+meu_decimal = 12.34
+print(type(meu_decimal))
+
+meu_booleano = True
+print(type(meu_booleano))
+
+meu_vazio = None
+print(type(meu_vazio))
+
+
+def minha_funcao():
+    meu_valor = 1234
+    return meu_valor
+
+print(minha_funcao())
+
+print(type(minha_funcao))
