ce52263859e46c1b321306d36df626c4e4f0742f
Lucas <lucassoares2007@hotmail.com>
2023-01-23 12:35:36 -0300

    perf

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-minha_string = 'Lucas'
-print(type(minha_string))
+nome = 'Lucas'
+idade = 26
+mensagem = f'Olá, meu nome é {nome} e eu tenho {idade} anos.'
+print(mensagem)
 
-meu_inteiro = 1234
-print(type(meu_inteiro))
-
-meu_decimal = 12.34
-print(type(meu_decimal))
-
-meu_booleano = True
-print(type(meu_booleano))
-
-meu_vazio = None
-print(type(meu_vazio))
-
-
-def minha_funcao():
-    meu_valor = 1234
-    return meu_valor
-
-print(minha_funcao())
-
-print(type(minha_funcao))
+x = 10
+y = 5
+resultado = f'{x} + {y} = {x + y}'
+print(resultado)
