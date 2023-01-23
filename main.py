bf5c81f0bf65a465a67d27744ff2e7be9dbeacdd
Lucas <lucassoares2007@hotmail.com>
2023-01-23 13:58:59 -0300

    perf

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-nome = 'Lucas'
-idade = 26
-mensagem = f'Olá, meu nome é {nome} e eu tenho {idade} anos.'
-print(mensagem)
+def delta(a, b, c):
+    return pow(b, 2) - 4 * a * c
 
-x = 10
-y = 5
-resultado = f'{x} + {y} = {x + y}'
-print(resultado)
+
+def bhaskara(a, b, c):
+    resul_delta = delta(a, b, c)
+
+    if resul_delta < 0:
+        return 'Delta Negativo'
+
+    raiz_delta = pow(resul_delta, 0.5)
+
+    x1 = (-b + raiz_delta)/(2*a)
+    x2 = (-b - raiz_delta)/(2*a)
+
+    return f'x1={round(x1,2)}, x2={round(x2,2)}'
+
+print(bhaskara(7, 3, 4))
+# Delta Negativo
+
+print(bhaskara(1, 5, 2))
+# x1=-0.44, x2=-4.56
+
+print(bhaskara(3, 10, 2))
+# x1=-0.21, x2=-3.12
