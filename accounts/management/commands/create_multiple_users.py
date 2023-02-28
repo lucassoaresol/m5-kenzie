8227059abfc09e65fdbf8980a9d74ada35aba9fe
Lucas <lucassoares2007@hotmail.com>
2023-02-28 00:57:37 -0300

    perf

---
+++ accounts/management/commands/create_multiple_users.py
@@ -0,0 +1,34 @@
+from accounts.models import Account
+from django.core.management.base import BaseCommand
+from django.utils.crypto import get_random_string
+
+
+class Command(BaseCommand):
+    help = "Create random users"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "total",
+            type=int,
+            help="Indicates the number of users to be created",
+        )
+        parser.add_argument(
+            "-p",
+            "--prefix",
+            type=str,
+            help="Define a username prefix",
+        )
+
+    def handle(self, *args, **kwargs):
+        total = kwargs["total"]
+        prefix = kwargs["prefix"]
+        for i in range(total):
+            if prefix:
+                username = f"{prefix}_{get_random_string(5)}"
+            else:
+                username = get_random_string(5)
+
+            Account.objects.create_user(
+                username=username,
+                password="123",
+            )
